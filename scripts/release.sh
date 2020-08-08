a0f5442218f849674b05661ddaee3b8891ddf885
Ash Midgley <ayelgdom@tutanota.com>
2020-08-08 17:40:46 +0100

    Add release script.

---
+++ scripts/release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "##### Installing dependencies"
+npm install
+echo "##### Building release"
+npm run build
+echo "##### Removing old files from release folder"
+rm -r /var/www/countriesofthe.world/html/*
+echo "##### Copying new build to release folder"
+cp -r ./build/* /var/www/countriesofthe.world/html
