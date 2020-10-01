0087a202856fae6da57fe3b039d909ab08766147
ash.midgley <ash.midgley@thecomplianceworkbook.com>
2020-10-01 02:53:07 +0100

    Add paging to leaderboard.

--- src/components/leaderboard/leaderboard.js
+++ src/components/leaderboard/leaderboard.js
@@ -10,19 +10,30 @@ class Leaderboard extends React.Component {
         this.state = {
             codes: null, 
             entries: null,
+            page: 0,
+            hasMore: false,
             loading: true
         };
     }
 
     componentDidMount() {
-        fetch(`${process.env.REACT_APP_API_URL}/leaderboard`)
+        this.getEntries(0);
+        this.getCodes();
+    }
+
+    getEntries = (page) => {
+        fetch(`${process.env.REACT_APP_API_URL}/leaderboard?page=${page}`)
             .then(response => response.json())
             .then(data => {
                 this.setState({
-                    entries: this.sortEntries(data)
+                    entries: data.entries,
+                    hasMore: data.hasMore,
+                    page: page
                 });
             });
+    }
 
+    getCodes = () => {
         fetch(`${process.env.REACT_APP_API_URL}/codes`)
             .then(response => response.json())
             .then(data => {
@@ -32,20 +43,20 @@ class Leaderboard extends React.Component {
             });
     }
 
-    sortEntries(entries) {
-        return entries.sort((left, right) => {
-            if(left.countries === right.countries)
-                return left.time > right.time;
-            return left.countries < right.countries;
-        });
-    }
-
     componentDidUpdate() {
         if(this.state.loading && this.state.entries && this.state.codes) {
             this.setState({
                 loading: false
             });
         }
+    }
+
+    pageBack = () => {
+        this.getEntries(this.state.page - 1);
+    }
+
+    pageForward = () => {
+        this.getEntries(this.state.page + 1);
     }
 
     render() {
@@ -75,7 +86,7 @@ class Leaderboard extends React.Component {
                             <tbody>
                                 {this.state.entries.map(entry =>
                                     <tr key={entry.id}>
-                                        <td>{this.state.entries.indexOf(entry)+1}</td>
+                                        <td>{(this.state.entries.indexOf(entry)+1)+(this.state.page*10)}</td>
                                         <td>{entry.name}</td>
                                         <td>
                                             <img id="flag" src={`/flags/${this.state.codes[entry.country]}.svg`} alt="Country flag"/>
@@ -86,6 +97,14 @@ class Leaderboard extends React.Component {
                                 )}
                             </tbody>
                         </table>
+                        <div className="float-right">
+                            <button className="btn" onClick={this.pageBack} disabled={this.state.page === 0}>
+                                <ion-icon name="caret-back-outline" ></ion-icon>
+                            </button>
+                            <button className="btn" onClick={this.pageForward} disabled={!this.state.hasMore}>
+                                <ion-icon name="caret-forward-outline"></ion-icon>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
